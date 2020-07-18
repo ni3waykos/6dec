47430edeac4b2077273ca474229438cada760b87
ni3waykos <65816064+ni3waykos@users.noreply.github.com>
2020-07-18 15:46:32 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'July 15, 2020',
+  birthdate: 'July 18, 2020',
   name: 'Muley Vikas'
 };
 
