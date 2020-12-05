62daecf47eb929c16fe2fb713913e82cd82d06d3
ni3waykos <65816064+ni3waykos@users.noreply.github.com>
2020-12-05 23:56:40 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'July 31, 2020',
-  name: 'Vikas Muley'
+  birthdate: 'December 6, 2020',
+  name: 'Amit Shirwat a.k.a Tillya'
 };
 
 function hideEverything() {
